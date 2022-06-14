9da1f3c5aa8ab26ee901a7160a304fa5f9dc8cf3
David Connet <dcon@agilityrecordbook.com>
2022-06-14 09:54:28 -0700

    Update comments

--- Include/LibARBWin/Logger.h
+++ Include/LibARBWin/Logger.h
@@ -24,18 +24,19 @@ public:
 	// Default logging level (currently set to wxLOG_User)
 	static wxLogLevelValues GetLogLevel();
 
+	// Wrapper for wxLogGeneric(GetLogLevel(), L"%s", msg);
 	static void Log(wxString const& msg);
 
 	CLogger();
 
 	// keep = 0 means keep all.
+	// Sets base logger to write to a file, adds a wxLogWindow
 	void EnableLogWindow(wxWindow* parent, bool show, wchar_t const* baseFilename, size_t keepNlogs = 5);
 
 	// These functions are only valid if logging has been enabled
 	wxString GetCurrentLogDir() const;
 	wxString GetCurrentLogName() const;
 	size_t FindExistingLogs(wxArrayString& files);
-
 	wxFrame* GetFrame() const;
 	void Show(bool show = true);
 

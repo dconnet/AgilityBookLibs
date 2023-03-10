448b3e1ee149bab07cf32c29bf5ece4fe138c170
David Connet <dcon@agilityrecordbook.com>
2023-03-10 15:15:14 -0800

    ARB 3.8.1

--- Include/VersionNumber.h
+++ Include/VersionNumber.h
@@ -21,7 +21,7 @@
 #define ARB_VER_MAJOR 3
 #define ARB_VER_MINOR 8
 #define ARB_VER_DOT   1
-#define ARB_VER_BUILD 118
+#define ARB_VER_BUILD 119
 
 // Number of parts in the caption version string
 #ifndef VER_PARTS

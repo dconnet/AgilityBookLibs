5998a03313501628d4d0eaaf1556a2664e91f35d
David Connet <dcon@agilityrecordbook.com>
2026-08-04 16:07:50 -0700

    v4.0.3.131 release

--- Include/VersionNumber.h
+++ Include/VersionNumber.h
@@ -21,7 +21,7 @@
 #define ARB_VER_MAJOR 4
 #define ARB_VER_MINOR 0
 #define ARB_VER_DOT   3
-#define ARB_VER_BUILD 130
+#define ARB_VER_BUILD 131
 
 // Number of parts in the caption version string
 #ifndef VER_PARTS
